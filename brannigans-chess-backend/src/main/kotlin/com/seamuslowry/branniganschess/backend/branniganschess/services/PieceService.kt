4f6edfa7702257f1f4e9ad290790e3ef18a7c8d4
Seamus Lowry <spl1owry3@gmail.com>
2020-09-19 14:45:41 -0400

    add searches for pieces

--- brannigans-chess-backend/src/main/kotlin/com/seamuslowry/branniganschess/backend/branniganschess/services/PieceService.kt
+++ brannigans-chess-backend/src/main/kotlin/com/seamuslowry/branniganschess/backend/branniganschess/services/PieceService.kt
@@ -6,6 +6,7 @@ import com.seamuslowry.branniganschess.backend.branniganschess.models.PieceColor
 import com.seamuslowry.branniganschess.backend.branniganschess.models.PieceType
 import com.seamuslowry.branniganschess.backend.branniganschess.repos.GameRepository
 import com.seamuslowry.branniganschess.backend.branniganschess.repos.PieceRepository
+import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -14,4 +15,33 @@ class PieceService (
         private val pieceRepository: PieceRepository
 ) {
     fun createPiece(p: Piece): Piece = pieceRepository.save(p)
+
+    fun findAllBy(gameId: Long, color: PieceColor?, taken: Boolean?): Iterable<Piece> {
+        var spec: Specification<Piece> = Specification.where(inGame(gameId))!!
+
+        color?.let { spec = spec.and(isColor(it))!! }
+
+        taken?.let { spec = spec.and(isTaken(it))!! }
+
+        return pieceRepository.findAll(spec)
+    }
+
+
+    private fun inGame(id: Long): Specification<Piece> = Specification {
+        root,
+        _,
+        criteriaBuilder -> criteriaBuilder.equal(root.get<Game>("game").get<Long>("id"), id)
+    }
+
+    private fun isColor(color: PieceColor): Specification<Piece> = Specification {
+        root,
+        _,
+        criteriaBuilder -> criteriaBuilder.equal(root.get<PieceColor>("color"), color.toString())
+    }
+
+    private fun isTaken(taken: Boolean): Specification<Piece> = Specification {
+        root,
+        _,
+        criteriaBuilder -> criteriaBuilder.equal(root.get<Boolean>("taken"), taken)
+    }
 }
