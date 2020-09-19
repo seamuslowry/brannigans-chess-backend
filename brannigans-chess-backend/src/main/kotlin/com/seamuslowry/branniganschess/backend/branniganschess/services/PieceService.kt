4f0b08c8ceefc4478d9c14d7297c731afcba8181
Seamus Lowry <spl1owry3@gmail.com>
2020-09-19 17:58:55 -0400

    misc and add itests for searching pieces

--- brannigans-chess-backend/src/main/kotlin/com/seamuslowry/branniganschess/backend/branniganschess/services/PieceService.kt
+++ brannigans-chess-backend/src/main/kotlin/com/seamuslowry/branniganschess/backend/branniganschess/services/PieceService.kt
@@ -3,12 +3,9 @@ package com.seamuslowry.branniganschess.backend.branniganschess.services
 import com.seamuslowry.branniganschess.backend.branniganschess.models.Game
 import com.seamuslowry.branniganschess.backend.branniganschess.models.Piece
 import com.seamuslowry.branniganschess.backend.branniganschess.models.PieceColor
-import com.seamuslowry.branniganschess.backend.branniganschess.models.PieceType
-import com.seamuslowry.branniganschess.backend.branniganschess.repos.GameRepository
 import com.seamuslowry.branniganschess.backend.branniganschess.repos.PieceRepository
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
 class PieceService (
