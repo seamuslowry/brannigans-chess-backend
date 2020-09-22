d0dfcbe87c258911c367d8efd815e92401e9d5aa
Seamus Lowry <spl1owry3@gmail.com>
2020-09-22 14:59:21 -0400

    add integration tests for moving

--- src/test/kotlin/com/seamuslowry/branniganschess/backend/branniganschess/integration/MoveIntegrationTests.kt
+++ src/test/kotlin/com/seamuslowry/branniganschess/backend/branniganschess/integration/MoveIntegrationTests.kt
@@ -3,8 +3,9 @@ package com.seamuslowry.branniganschess.backend.branniganschess.integration
 import com.seamuslowry.branniganschess.backend.branniganschess.dtos.MoveRequest
 import com.seamuslowry.branniganschess.backend.branniganschess.models.Game
 import com.seamuslowry.branniganschess.backend.branniganschess.models.Move
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
+import com.seamuslowry.branniganschess.backend.branniganschess.services.GameService
+import com.seamuslowry.branniganschess.backend.branniganschess.services.PieceService
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,16 +14,56 @@ import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class MoveIntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+class MoveIntegrationTests(
+        @Autowired val restTemplate: TestRestTemplate,
+        @Autowired val gameService: GameService,
+        @Autowired val pieceService: PieceService
+) {
 
     @Test
-    fun `Throws an exception when trying to move`() {
+    fun `Throws an exception on an invalid move`() {
+        val game = createGame()
         val response = restTemplate.postForEntity(
-                "/moves/1",
+                "/moves/${game.id}",
                 MoveRequest(0,0,0,0),
                 String::class.java
         )
 
         assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
     }
+
+    @Test
+    fun `moves a piece`() {
+        val game = createGame()
+        val response = restTemplate.postForEntity(
+                "/moves/${game.id}",
+                MoveRequest(1,0,2,0),
+                Move::class.java
+        )
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertNotNull(response.body?.movingPiece)
+        assertNull(response.body?.takenPiece)
+    }
+
+    @Test
+    fun `takes a piece`() {
+        val game = createGame()
+        val board = pieceService.getPiecesAsBoard(game.id)
+
+        // set up a valid test by performing an invalid move through the service
+        pieceService.movePiece(board[0][0]!!, 4,0)
+
+        val response = restTemplate.postForEntity(
+                "/moves/${game.id}",
+                MoveRequest(4,0,6,0),
+                Move::class.java
+        )
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertNotNull(response.body?.movingPiece)
+        assertNotNull(response.body?.takenPiece)
+    }
+
+    private fun createGame(): Game = gameService.createGame()
 }
