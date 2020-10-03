eeb38c2d73227ee6cc878158e1bf8be10c815b8e
Seamus Lowry <spl1owry3@gmail.com>
2020-10-03 09:57:09 -0400

    feature/bishop moves (#47)
    * TDD
    
    * add bishop moves and captures

---
+++ src/test/kotlin/com/seamuslowry/branniganschess/backend/models/pieces/BishopTest.kt
@@ -0,0 +1,58 @@
+package com.seamuslowry.branniganschess.backend.models.pieces
+
+import com.seamuslowry.branniganschess.backend.models.Game
+import com.seamuslowry.branniganschess.backend.models.PieceColor
+import com.seamuslowry.branniganschess.backend.models.Position
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class BishopTest {
+    @Test
+    fun `can move along diagonals`() {
+        val game = Game("New Game")
+        val piece = Bishop(PieceColor.BLACK, game, 3, 3)
+
+        assertTrue(piece.canMove(Position(2,2)))
+        assertTrue(piece.canMove(Position(2,4)))
+        assertTrue(piece.canMove(Position(4,2)))
+        assertTrue(piece.canMove(Position(4,4)))
+    }
+
+    @Test
+    fun `cannot move off the board`() {
+        val game = Game("New Game")
+        val piece = Bishop(PieceColor.BLACK, game, 0, 0)
+
+        assertFalse(piece.canMove(Position(-1,-1)))
+    }
+
+    @Test
+    fun `captures where it can move`() {
+        val game = Game("New Game")
+        val piece = Bishop(PieceColor.BLACK, game, 3, 3)
+
+        assertTrue(piece.canCapture(Position(2,2)))
+        assertTrue(piece.canCapture(Position(2,4)))
+        assertTrue(piece.canCapture(Position(4,2)))
+        assertTrue(piece.canCapture(Position(4,4)))
+    }
+
+    @Test
+    fun `requires fields along the diagonal it would travel be empty`() {
+        val game = Game("New Game")
+        val piece = Bishop(PieceColor.BLACK, game, 0, 0)
+
+        val requiresEmpty = piece.requiresEmpty(Position(7,7))
+        assertEquals(setOf(
+                Position(1,1),
+                Position(2,2),
+                Position(3,3),
+                Position(4,4),
+                Position(5,5),
+                Position(6,6)
+        ), requiresEmpty)
+    }
+}
