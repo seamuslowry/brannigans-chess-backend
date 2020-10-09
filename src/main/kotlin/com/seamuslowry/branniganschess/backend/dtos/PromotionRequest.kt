0fb6f8133a947e609c0f5d231573661abd53066b
Seamus Lowry <spl1owry3@gmail.com>
2020-10-08 20:51:30 -0400

    add DTO

--- src/main/kotlin/com/seamuslowry/branniganschess/backend/dtos/PromotionRequest.kt
+++ src/main/kotlin/com/seamuslowry/branniganschess/backend/dtos/PromotionRequest.kt
@@ -1,2 +1,9 @@
 package com.seamuslowry.branniganschess.backend.dtos
 
+import com.seamuslowry.branniganschess.backend.models.PieceType
+
+data class PromotionRequest (
+        val row: Int,
+        val col: Int,
+        val type: PieceType
+)
