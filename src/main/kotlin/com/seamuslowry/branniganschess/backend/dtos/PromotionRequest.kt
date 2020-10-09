35bae090a58706d1f4091e5cbf53ce2c3003c2a8
Seamus Lowry <spl1owry3@gmail.com>
2020-10-08 21:12:46 -0400

    delete unused request

--- src/main/kotlin/com/seamuslowry/branniganschess/backend/dtos/PromotionRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.seamuslowry.branniganschess.backend.dtos
-
-import com.seamuslowry.branniganschess.backend.models.PieceType
-
-data class PromotionRequest (
-        val row: Int,
-        val col: Int,
-        val type: PieceType
-)
