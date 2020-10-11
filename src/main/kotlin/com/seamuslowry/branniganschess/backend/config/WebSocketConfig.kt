48bcd279db6c0eafa7cc1f8a978e95f29b08045b
Seamus Lowry <spl1owry3@gmail.com>
2020-10-11 18:42:38 -0400

    POC web socket

---
+++ src/main/kotlin/com/seamuslowry/branniganschess/backend/config/WebSocketConfig.kt
@@ -0,0 +1,26 @@
+package com.seamuslowry.branniganschess.backend.config
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Component
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/game")
+                .setAllowedOrigins("*") // TODO use correct allowed origins
+                .withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker(MESSAGE_PREFIX)
+        registry.setApplicationDestinationPrefixes("/gameStatus")
+    }
+
+    companion object {
+        const val MESSAGE_PREFIX = "/topic"
+    }
+}
