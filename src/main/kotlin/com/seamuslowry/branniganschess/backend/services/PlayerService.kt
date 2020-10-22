b0dccc5f8f62a95ceadde7019044249f90b2604d
Seamus Lowry <spl1owry3@gmail.com>
2020-10-22 17:13:42 -0400

    add forgotten

---
+++ src/main/kotlin/com/seamuslowry/branniganschess/backend/services/PlayerService.kt
@@ -0,0 +1,39 @@
+package com.seamuslowry.branniganschess.backend.services
+
+import com.seamuslowry.branniganschess.backend.models.*
+import com.seamuslowry.branniganschess.backend.repos.PlayerRepository
+import org.springframework.data.jpa.domain.Specification
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.stereotype.Service
+
+@Service
+class PlayerService (
+        private val playerRepository: PlayerRepository,
+        private val gameService: GameService
+) {
+    fun getOrCreate(authId: String): Player {
+        var player: Player?
+
+        try {
+            player = getByAuthId(authId)
+            player = player ?: playerRepository.save(Player(authId))
+        } catch (e: Exception) {
+            player = getByAuthId(authId)
+        }
+
+        return player ?: throw NoSuchElementException("Player cannot be found or created.")
+    }
+
+    fun getGames(player: Player, color: PieceColor?, active: Boolean?) = gameService.findPlayerGames(player, color, active)
+
+    private fun getByAuthId(authId: String): Player? = playerRepository.findOne(Specification.where(withAuthId(authId))).orElse(null)
+
+    private fun getFromJwt(jwt: Jwt): Player? = playerRepository.findOne(Specification.where(withAuthId(jwt.subject))).orElse(null)
+
+
+    private fun withAuthId(authId: String): Specification<Player> = Specification {
+        root,
+        _,
+        criteriaBuilder -> criteriaBuilder.equal(root.get<Player>("authId"), authId)
+    }
+}
