c1a46431cd32832d8c1a67995e753c5c46eeabcc
Seamus Lowry <spl1owry3@gmail.com>
2020-10-22 21:22:14 -0400

    create game test fixes

--- src/test/kotlin/com/seamuslowry/branniganschess/backend/integration/CreateGameIntegrationTest.kt
+++ src/test/kotlin/com/seamuslowry/branniganschess/backend/integration/CreateGameIntegrationTest.kt
@@ -1,23 +1,33 @@
 package com.seamuslowry.branniganschess.backend.integration
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.seamuslowry.branniganschess.backend.dtos.MoveRequest
 import com.seamuslowry.branniganschess.backend.models.Game
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class CreateGameIntegrationTest(@Autowired val restTemplate: TestRestTemplate) {
+@AutoConfigureMockMvc
+class CreateGameIntegrationTest(@Autowired val mockMvc: MockMvc) {
 
     @Test
     fun `Create a new game`() {
-        val entity = restTemplate.exchange("/games/create", HttpMethod.POST, null, Game::class.java)
-
-        assertEquals(HttpStatus.OK, entity.statusCode)
-        assertNotNull(entity.body?.id)
+        mockMvc.post("/games/create") {
+            with(SecurityMockMvcRequestPostProcessors.jwt())
+        }.andExpect {
+            status { isOk }
+            jsonPath("id") { isNotEmpty }
+        }
     }
 }
