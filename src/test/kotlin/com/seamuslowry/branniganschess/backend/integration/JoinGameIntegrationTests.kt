59dfe06641c476452756514793c5c58e0556d16f
Seamus Lowry <spl1owry3@gmail.com>
2020-10-31 12:42:03 -0400

    add itests

---
+++ src/test/kotlin/com/seamuslowry/branniganschess/backend/integration/JoinGameIntegrationTests.kt
@@ -0,0 +1,77 @@
+package com.seamuslowry.branniganschess.backend.integration
+
+import com.seamuslowry.branniganschess.backend.models.GameStatus
+import com.seamuslowry.branniganschess.backend.services.GameService
+import com.seamuslowry.branniganschess.backend.services.PlayerService
+import org.junit.jupiter.api.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class JoinGameIntegrationTests(
+    @Autowired val mockMvc: MockMvc,
+    @Autowired val playerService: PlayerService,
+    @Autowired val gameService: GameService
+) {
+    @Test
+    fun `will join a new game`() {
+        val playerAuthId = "join-game-as-white-itest"
+        val player = playerService.getPlayer(playerAuthId)
+        val game = gameService.createGame()
+
+        mockMvc.post("/players/join/${game.id}") {
+            with(jwt().jwt { it.claim("sub", player.authId) })
+        }.andExpect {
+            status { isOk }
+            jsonPath("whitePlayer") { isNotEmpty }
+            jsonPath("blackPlayer") { isEmpty }
+            jsonPath("status") { value(GameStatus.WAITING_FOR_BLACK.toString()) }
+            jsonPath("whitePlayer.authId") { value(playerAuthId) }
+        }
+    }
+
+    @Test
+    fun `will join a new game as black`() {
+        val playerAuthId = "join-game-as-black-itest"
+        val player = playerService.getPlayer(playerAuthId)
+        val game = gameService.createGame()
+
+        mockMvc.post("/players/join/${game.id}?color=BLACK") {
+            with(jwt().jwt { it.claim("sub", player.authId) })
+        }.andExpect {
+            status { isOk }
+            jsonPath("whitePlayer") { isEmpty }
+            jsonPath("blackPlayer") { isNotEmpty }
+            jsonPath("status") { value(GameStatus.WAITING_FOR_WHITE.toString()) }
+            jsonPath("blackPlayer.authId") { value(playerAuthId) }
+        }
+    }
+
+    @Test
+    fun `filling a game starts play`() {
+        val blackPlayerAuthId = "black-fill-game-itest"
+        val whitePlayerAuthId = "white-fill-game-itest"
+        val blackPlayer = playerService.getPlayer(blackPlayerAuthId)
+        val whitePlayer = playerService.getPlayer(whitePlayerAuthId)
+        val game = gameService.createGame()
+
+        mockMvc.post("/players/join/${game.id}") {
+            with(jwt().jwt { it.claim("sub", whitePlayer.authId) })
+        }.andExpect {
+            status { isOk }
+            jsonPath("status") { value(GameStatus.WAITING_FOR_BLACK.toString()) }
+        }
+
+        mockMvc.post("/players/join/${game.id}") {
+            with(jwt().jwt { it.claim("sub", blackPlayer.authId) })
+        }.andExpect {
+            status { isOk }
+            jsonPath("status") { value(GameStatus.WHITE_TURN.toString()) }
+        }
+    }
+}
---
+++ src/test/kotlin/com/seamuslowry/branniganschess/backend/integration/LeaveGameIntegrationTests.kt
@@ -0,0 +1,73 @@
+package com.seamuslowry.branniganschess.backend.integration
+
+import com.seamuslowry.branniganschess.backend.models.GameStatus
+import com.seamuslowry.branniganschess.backend.services.GameService
+import com.seamuslowry.branniganschess.backend.services.PlayerService
+import org.junit.jupiter.api.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class LeaveGameIntegrationTests(
+    @Autowired val mockMvc: MockMvc,
+    @Autowired val playerService: PlayerService,
+    @Autowired val gameService: GameService
+) {
+    @Test
+    fun `can leave a game with one player`() {
+        val playerAuthId = "leave-game-itest"
+        val player = playerService.getPlayer(playerAuthId)
+        val game = gameService.createGame()
+
+        mockMvc.post("/players/join/${game.id}") {
+            with(jwt().jwt { it.claim("sub", player.authId) })
+        }.andExpect {
+            status { isOk }
+        }
+
+        mockMvc.post("/players/leave/${game.id}") {
+            with(jwt().jwt { it.claim("sub", player.authId) })
+        }.andExpect {
+            status { isOk }
+            jsonPath("status") { value(GameStatus.WAITING_FOR_PLAYERS.toString()) }
+        }
+    }
+
+    @Test
+    fun `cannot leave a game with two players`() {
+        val playerOneAuthId = "cannot-leave-itest-one"
+        val playerTwoAuthId = "cannot-leave-itest-two"
+        val playerOne = playerService.getPlayer(playerOneAuthId)
+        val playerTwo = playerService.getPlayer(playerTwoAuthId)
+        val game = gameService.createGame()
+
+        mockMvc.post("/players/join/${game.id}") {
+            with(jwt().jwt { it.claim("sub", playerOne.authId) })
+        }.andExpect {
+            status { isOk }
+        }
+
+        mockMvc.post("/players/join/${game.id}") {
+            with(jwt().jwt { it.claim("sub", playerTwo.authId) })
+        }.andExpect {
+            status { isOk }
+        }
+
+        mockMvc.post("/players/leave/${game.id}") {
+            with(jwt().jwt { it.claim("sub", playerOne.authId) })
+        }.andExpect {
+            status { isConflict }
+        }
+
+        mockMvc.post("/players/leave/${game.id}") {
+            with(jwt().jwt { it.claim("sub", playerTwo.authId) })
+        }.andExpect {
+            status { isConflict }
+        }
+    }
+}
