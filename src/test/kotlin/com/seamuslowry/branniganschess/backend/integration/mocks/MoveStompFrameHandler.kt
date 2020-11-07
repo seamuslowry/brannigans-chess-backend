2fe34cc84d7995946f0370284abc42bafb9e02e6
Seamus Lowry <spl1owry3@gmail.com>
2020-11-07 12:46:56 -0500

    chore/test cleanup (#76)
    * not null in handler
    
    * not null in tests

--- src/test/kotlin/com/seamuslowry/branniganschess/backend/integration/mocks/MoveStompFrameHandler.kt
+++ src/test/kotlin/com/seamuslowry/branniganschess/backend/integration/mocks/MoveStompFrameHandler.kt
@@ -5,10 +5,10 @@ import org.springframework.messaging.simp.stomp.StompFrameHandler
 import org.springframework.messaging.simp.stomp.StompHeaders
 import java.lang.reflect.Type
 
-class MoveStompFrameHandler(private val messages: MutableList<Move?>) : StompFrameHandler {
+class MoveStompFrameHandler(private val messages: MutableList<Move>) : StompFrameHandler {
     override fun getPayloadType(headers: StompHeaders): Type = Move::class.java
 
     override fun handleFrame(headers: StompHeaders, payload: Any?) {
-        messages.add(payload as Move?)
+        messages.add(payload as Move)
     }
 }
