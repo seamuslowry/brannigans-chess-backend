a1bc19fa67833261773d90ad18cee2238f0ec452
Seamus Lowry <spl1owry3@gmail.com>
2021-04-28 16:51:29 -0400

    imports

--- src/main/kotlin/com/seamuslowry/branniganschess/backend/config/SwaggerConfig.kt
+++ src/main/kotlin/com/seamuslowry/branniganschess/backend/config/SwaggerConfig.kt
@@ -2,10 +2,15 @@ package com.seamuslowry.branniganschess.backend.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.RestController
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
