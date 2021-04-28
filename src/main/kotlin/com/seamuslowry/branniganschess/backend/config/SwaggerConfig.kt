850a18bda9404b09ea7fbe5300a51ed1a801ae80
Seamus Lowry <spl1owry3@gmail.com>
2021-04-28 16:59:00 -0400

    feature/auth in swagger (#98)
    * imports
    
    * extra methods

--- src/main/kotlin/com/seamuslowry/branniganschess/backend/config/SwaggerConfig.kt
+++ src/main/kotlin/com/seamuslowry/branniganschess/backend/config/SwaggerConfig.kt
@@ -2,10 +2,15 @@ package com.seamuslowry.branniganschess.backend.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.RestController
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
@@ -15,9 +20,17 @@ class SwaggerConfig {
     @Bean
     fun apiDocket(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(listOf(securityContext()))
+                .securitySchemes(listOf(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController::class.java))
                 .paths(PathSelectors.any())
                 .build()
     }
+
+    private fun apiKey() = ApiKey("JWT", HttpHeaders.AUTHORIZATION, "header")
+
+    private fun securityContext(): SecurityContext = SecurityContext.builder().securityReferences(defaultAuth()).build()
+
+    private fun defaultAuth() = listOf(SecurityReference("JWT", arrayOf(AuthorizationScope("global", "accessEverything"))))
 }
